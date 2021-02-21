8c649b73ba1327656ab82511f3b8b82c7435b058
Alejandro Gonzalez <intuido@yahoo.com>
2021-02-21 14:00:26 -0300

    Added web app (empty) and dev tests

---
+++ Tests/Futbol.Seasons.DevTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.SimpleNotificationService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Futbol.Seasons.DevTest
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            //using IHost host = CreateHostBuilder(args).Build();
+
+            // Application code should start here.
+            //var snsClient = new AmazonSimpleNotificationServiceClient(credentials, region);
+
+            //await host.RunAsync();
+        }
+
+        //static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args);
+       
+
+        public static IConfiguration LoadConfiguration()
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env}.json", true, true)
+                .AddEnvironmentVariables().AddUserSecrets<Program>();
+
+            return builder.Build();
+        }
+    }
+}
